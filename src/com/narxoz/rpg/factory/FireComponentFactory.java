4f5395c29fdc5f39c4730f005dc95be680bb9c92
SaoMadao <zhassulan.estai@narxoz.kz>
2026-02-21 10:55:35 +0500

    Добавил Factory классы

---
+++ src/com/narxoz/rpg/factory/FireComponentFactory.java
@@ -0,0 +1,17 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.*;
+import com.narxoz.rpg.loot.*;
+import java.util.*;
+
+public class FireComponentFactory implements EnemyComponentFactory {
+    @Override public List<Ability> createAbilities() {
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new FlameBreath());
+        abilities.add(new FireShield());
+        abilities.add(new MeteorStorm());
+        return abilities;
+    }
+    @Override public LootTable createLootTable() { return new FireLootTable(); }
+    @Override public String createAIBehavior() { return "AGGRESSIVE"; }
+}
---
+++ src/com/narxoz/rpg/factory/IceComponentFactory.java
@@ -0,0 +1,17 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.*;
+import com.narxoz.rpg.loot.*;
+import java.util.*;
+
+public class IceComponentFactory implements EnemyComponentFactory {
+    @Override public List<Ability> createAbilities() {
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new FrostBreath());
+        abilities.add(new IceShield());
+        abilities.add(new Blizzard());
+        return abilities;
+    }
+    @Override public LootTable createLootTable() { return new IceLootTable(); }
+    @Override public String createAIBehavior() { return "DEFENSIVE"; }
+}
